acb10f5ff492d5af5648159962b75e2e06c15e5a
awinash <awinashgoswami3@gmail.com>
2023-03-13 10:31:26 +0500

    pagination applied

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,5 +1,20 @@
 import { comments } from "./../../../../data/comments";
 
 export default function handler(req, res) {
-  res.status(200).json(comments);
+  // GET Request
+  if (req.method === "GET") {
+    res.status(200).json(comments);
+  }
+  // POST Request
+  else if (req.method === "POST") {
+    const comment = req.body.comment;
+    const newComment = {
+      id: Date.now(),
+      text: comment,
+    };
+    comments.push(newComment);
+    res.status(201).json("New Comment Added", newComment);
+  }
 }
+
+// POST Request
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,21 +1,63 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usePagination, Pagination } from "pagination-react-js"
+
 
 export default function PostList({ posts }) {
+  const { currentPage, entriesPerPage, entries } = usePagination(1, 10)
+  const [postdata, setPostData] = useState([]);
+
+  useEffect(() => {
+    setPostData([...posts])
+  }, [])
   return (
     <>
-      <h1>List of Posts</h1>
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <Link href={`posts/${post.id}`} passHref>
-              <h2>
-                {post.id} {post.title}
-              </h2>
-            </Link>
-          </div>
-        );
-      })}
+      <div className="container">
+        <table className="table mt-5">
+          <tbody>
+            {
+              postdata.slice(entries.indexOfFirst, entries.indexOfLast).map(post => {
+                return (
+                  <tr key={post.id}>
+                    <td>
+                      <Link href={`posts/${post.id}`} passHref style={{ textDecoration: 'none', color: '#000' }}>
+                        <h2>
+                          {post.id}. {`${post.title}`.toUpperCase()}
+                        </h2>
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+
+        <Pagination
+          entriesPerPage={entriesPerPage.get}
+          totalEntries={postdata.length}
+          currentPage={{ get: currentPage.get, set: currentPage.set }}
+          offset={3}
+          classNames={{
+            wrapper: "pagination m-auto",
+            item: "pagination-item",
+            itemActive: "pagination-item-active",
+            navPrev: "pagination-item nav-item",
+            navNext: "pagination-item nav-item",
+            navStart: "pagination-item nav-item",
+            navEnd: "pagination-item nav-item",
+            navPrevCustom: "pagination-item",
+            navNextCustom: "pagination-item"
+          }}
+          showFirstNumberAlways={true}
+          showLastNumberAlways={true}
+          navStart="&#171;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+          navEnd="&#187;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+          navPrev="&#x2039;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+          navNext="&#x203a;" // Here you can pass anything (Text, HTML Tag, React Component, ...)
+          navPrevCustom={{ steps: 5, content: "\u00B7\u00B7\u00B7" /* Here you can pass anything (Text, HTML Tag, React Component, ...) */ }}
+          navNextCustom={{ steps: 5, content: "\u00B7\u00B7\u00B7" /* Here you can pass anything (Text, HTML Tag, React Component, ...) */ }}
+        />
+      </div>
     </>
   );
 }
